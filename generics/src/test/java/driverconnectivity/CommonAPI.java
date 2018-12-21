7e3ee7b627adfdd36fb790d3501abbae19783493
aaafnan <aaafnan@hotmail.com>
2018-12-21 00:16:33 -0500

    Group Framework

---
+++ generics/src/test/java/driverconnectivity/CommonAPI.java
@@ -0,0 +1,57 @@
+package driverconnectivity;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class CommonAPI {
+
+    protected WebDriver driver = null;
+
+    @Parameters({/*"useCloudEnv","cloudEnvName", */"os",  "browserName", "browserVersion", "url"})
+    @BeforeTest
+    public void setUp(@Optional("chrome") String browserName, @Optional("windows") String os, @Optional("http://automationpractice.com/index") String url, @Optional("74") String browserVersion)throws IOException {
+
+        getLocalDriver(browserName,os);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+
+        driver.get(url);
+
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+
+    }
+
+
+
+    public WebDriver getLocalDriver(String browserName, String os) {
+        if (browserName.equalsIgnoreCase("chrome")) {
+            if (os.equalsIgnoreCase("windows")) {
+                System.setProperty("webdriver.chrome.driver", "C:\\Users\\Saeed\\Desktop\\GroupProject\\generics\\drivers\\chromedriver.exe");
+                driver = new ChromeDriver();
+            } else if (os.equalsIgnoreCase("mac")) {
+                System.setProperty("webdriver.chrome.driver", "C:\\Users\\Saeed\\Desktop\\GroupProject\\generics\\drivers\\chromedriver");
+                driver = new ChromeDriver();
+            }
+        } else if (browserName.equalsIgnoreCase("firefox")) {
+            if (os.equalsIgnoreCase("windows")) {
+                System.setProperty("webdriver.gecko.driver", "C:\\Users\\Saeed\\Desktop\\GroupProject\\generics\\drivers\\geckodriver.exe");
+                driver = new ChromeDriver();
+            } else if (os.equalsIgnoreCase("mac")) {
+                System.setProperty("webdriver.gecko.driver", "NOT USING MAC ;(");
+                driver = new ChromeDriver();
+            }
+        }
+        return driver;
+    }
+
+    @AfterTest
+    public void closeOut(){
+        driver.manage().deleteAllCookies();
+        driver.quit();
+    }
+
+}
