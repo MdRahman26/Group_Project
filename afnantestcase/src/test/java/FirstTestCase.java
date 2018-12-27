b0f5770671f5d59e83fd1b6bc78af6e54aa6b1d7
aaafnan <aaafnan@hotmail.com>
2018-12-27 13:28:42 -0500

    removing generics source folder

---
+++ afnantestcase/src/test/java/FirstTestCase.java
@@ -0,0 +1,12 @@
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+import driverconnectivity.CommonAPI;
+public class FirstTestCase extends CommonAPI {
+
+    @Test
+    public void firstTest(){
+        System.out.println("Hello");
+
+
+    }
+}
---
+++ afnantestcase/src/test/java/SecondTEST.java
@@ -0,0 +1,11 @@
+import driverconnectivity.CommonAPI;
+import org.testng.annotations.Test;
+
+public class SecondTEST extends CommonAPI {
+
+
+    @Test
+    public void merTest(){
+
+    }
+}
